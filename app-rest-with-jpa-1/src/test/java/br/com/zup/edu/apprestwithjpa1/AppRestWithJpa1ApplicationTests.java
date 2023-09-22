6ca399d0eedc4744b9b61498be15fb9f48272a74
rafael.ponte@zup.com.br <rafael.ponte@zup.com.br>
2023-09-22 23:21:12 +0000

    stk-cli: create app initial commit.

---
+++ app-rest-with-jpa-1/src/test/java/br/com/zup/edu/apprestwithjpa1/AppRestWithJpa1ApplicationTests.java
@@ -0,0 +1,15 @@
+package br.com.zup.edu.apprestwithjpa1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("test")
+class AppRestWithJpa1ApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
